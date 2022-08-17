1acc0df28cf4449e507fa31cf7f51683e9b45790
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-08-17 10:34:38 -0300

    get product by id and return status

--- puertas/Controllers/ProductsController.cs
+++ puertas/Controllers/ProductsController.cs
@@ -20,5 +20,17 @@ namespace puertas.Controllers
             var products = repository.GetProducts();
             return products;
         }
+
+        //GET /product/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(Guid id){
+            //ActionResult permite retornar o objeto encontrado ou o status
+            var product = repository.GetProduct(id);
+            if (product is null){
+                return NotFound();
+            }
+            return Ok(product);
+            //return product; aceitavel tbm
+        }
     }
 }
