12d586bf9139bae769f22ae38b4fdcd1b4438309
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-08-17 15:42:28 -0300

    annotation required

--- puertas/Dtos/CreateProductDto.cs
+++ puertas/Dtos/CreateProductDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace puertas.Dtos{
     public record CreateProductDto{
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 10000)]
         public decimal Price { get; set; }
         public string Description { get; set; } = string.Empty;
-        
+
     }
 }
